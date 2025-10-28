396471728b8573d2021838a819ef896b3d534432
luckas-lopes <luckas.silva@sptech.school>
2025-10-28 20:04:16 -0300

    Validações da tela de cadastro retrabalhadas

--- public/js/cadastro.js
+++ public/js/cadastro.js
@@ -1,8 +1,7 @@
 let listaAgenciasCadastradas = [];
 
-
 function cadastrar() {
-    
+
     let nome = nome_value.value;
     let email = email_value.value;
     let senha = senha_value.value;
@@ -20,21 +19,30 @@ function cadastrar() {
             codigoEmpresaServer: codigoEmpresa
         }),
     })
-    .then(function(resposta){
-        console.log("resposta: ", resposta);
+        .then(function (resposta) {
+            console.log("resposta: ", resposta);
 
-        if(resposta.ok) {
-            alert("Cadastro realizado com sucesso! Redirecionando para tela de Login...");
-            setTimeout(() => {
-                window.location = "login.html";
-            }, "2000");
-        } else{
-            throw "Houve um erro ao tentar realizar o cadastro!";
-        }
-    })
-    .catch(function(resposta) {
-        console.log(`#ERRO: ${resposta}`);
-    });
+            if (resposta.ok) {
+                botao.disabled = true;
+                
+                document.getElementById("botao").style.opacity = (0.8);
+                document.getElementById("botao").style.cursor = ("default");
+                document.getElementById("div_mensagem").style.display = ("flex");
+                document.getElementById("div_mensagem").style.color = ("#49c427ff");
+                div_mensagem.innerHTML = "Cadastro realizado com sucesso! Redirecionando para tela de Login...";
+                setTimeout(() => {
+                    window.location = "login.html";
+                }, "2000");
+            } else {
+                document.getElementById("div_mensagem").style.display = ("flex");
+                document.getElementById("div_mensagem").style.color = ("red");
+                div_mensagem.innerHTML = "Erro ao realizar login! Verifique suas credenciais";
+                throw "Houve um erro ao tentar realizar o cadastro!";
+            }
+        })
+        .catch(function (resposta) {
+            console.log(`#ERRO: ${resposta}`);
+        });
 
     return false;
 }
