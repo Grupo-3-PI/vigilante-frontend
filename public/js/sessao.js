fe3fb53976b436daae3d3f44f9acc3de2343072b
GuiAndradeGap <gapguilherme.81@gmail.com>
2025-11-16 12:01:14 -0300

    Padronizando navbar lateral no site

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -1,32 +1,32 @@
-function validarSessao() {
-    // Recupera os dados da sessionStorage usando getItem
-    var email = sessionStorage.getItem("EMAIL_USUARIO");
-    var nome = sessionStorage.getItem("NOME_USUARIO");
+// function validarSessao() {
+//     // Recupera os dados da sessionStorage usando getItem
+//     var email = sessionStorage.getItem("EMAIL_USUARIO");
+//     var nome = sessionStorage.getItem("NOME_USUARIO");
 
-    // Encontra o elemento <span> no header
-    var b_usuario = document.getElementById("b_usuario");
+//     // Encontra o elemento <span> no header
+//     var b_usuario = document.getElementById("b_usuario");
 
-    // Validação:
-    // Se o email ou o nome não existirem (forem nulos),
-    // significa que o usuário não está logado.
-    if (email == null || nome == null) {
+//     // Validação:
+//     // Se o email ou o nome não existirem (forem nulos),
+//     // significa que o usuário não está logado.
+//     if (email == null || nome == null) {
         
-        // Limpa qualquer lixo que possa ter ficado
-        sessionStorage.clear();
+//         // Limpa qualquer lixo que possa ter ficado
+//         sessionStorage.clear();
         
-        // Avisa o usuário e o expulsa para a tela de login
-        alert("Sua sessão expirou ou você não está logado. Por favor, faça login novamente.");
-        window.location = "login.html"; 
+//         // Avisa o usuário e o expulsa para a tela de login
+//         alert("Sua sessão expirou ou você não está logado. Por favor, faça login novamente.");
+//         window.location = "login.html"; 
     
-    } else {
+//     } else {
         
-        // Se o usuário está logado:
-        if (b_usuario != null) {
-            // Se existir, preenche com a saudação
-            b_usuario.innerHTML = nome;
-        }
-    }
-}
+//         // Se o usuário está logado:
+//         if (b_usuario != null) {
+//             // Se existir, preenche com a saudação
+//             b_usuario.innerHTML = nome;
+//         }
+//     }
+// }
 
 // Função para limpar a sessão (botão "Sair")
 function sair() {
