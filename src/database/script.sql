4eeb0698e78077b83333e1fc427e62c5aecc69e5
Nandoz7 <luis.augusto@sptech.school>
2025-11-21 20:55:21 -0300

    adaptando novo bd

--- src/database/script.sql
+++ src/database/script.sql
@@ -1,19 +1,19 @@
-CREATE DATABASE PrevCrime_Vigilante;
+CREATE DATABASE IF NOT EXISTS PrevCrime_Vigilante;
 USE PrevCrime_Vigilante;
 
 CREATE TABLE EnderecoAgencia (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	cep CHAR(8),
-    rua VARCHAR(45),
-    bairro VARCHAR(45),
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    cep VARCHAR(8),
+    rua VARCHAR(50),
+    bairro VARCHAR(40),
     cidade VARCHAR(45),
     estado VARCHAR(20)
 );
 
 CREATE TABLE PlanoAssinatura (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-    descricao VARCHAR(45),
-    data_assinatura DATETIME
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    descricao VARCHAR(30),
+    data_assinatura DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE Agencia (
@@ -25,19 +25,20 @@ CREATE TABLE Agencia (
     codigo_empresa VARCHAR(45),
     fk_endereco_agencia INT,
     fk_plano_assinatura INT,
-	FOREIGN KEY (fk_endereco_agencia) REFERENCES EnderecoAgencia(id),
-	FOREIGN KEY (fk_plano_assinatura) REFERENCES PlanoAssinatura(id)
+    FOREIGN KEY (fk_endereco_agencia) REFERENCES EnderecoAgencia(id),
+    FOREIGN KEY (fk_plano_assinatura) REFERENCES PlanoAssinatura(id)
 );
 
 INSERT INTO Agencia (nome, email, senha, codigo_empresa) VALUES
 ('CVC', 'cvc@email.com', 'cvc.123', '1');
 
-CREATE TABLE NotificacaoSlack (
+CREATE TABLE AvisosSlack (
     id INT AUTO_INCREMENT PRIMARY KEY,
     titulo VARCHAR(45),
-    mensagem VARCHAR(100),
+    mensagem VARCHAR(60),
     tipo_notificacao VARCHAR(20),
-    data_envio DATETIME
+    data_envio DATETIME,
+    status VARCHAR(20)
 );
 
 CREATE TABLE Usuario (
@@ -47,21 +48,24 @@ CREATE TABLE Usuario (
     senha VARCHAR(100) NOT NULL,
     dt_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
     cargo_agencia VARCHAR(45),
-    status VARCHAR(20) NOT NULL,
     fk_agencia INT,
     fk_notificacao_slack INT,
-    fk_representante INT,
     FOREIGN KEY (fk_agencia) REFERENCES Agencia(id),
-    FOREIGN KEY (fk_notificacao_slack) REFERENCES NotificacaoSlack(id),
-    FOREIGN KEY (fk_representante) REFERENCES Usuario(id)
+    FOREIGN KEY (fk_notificacao_slack) REFERENCES AvisosSlack(id)
 );
 
-CREATE TABLE Relatorio (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-    titulo_relatorio VARCHAR(45),
-    dt_geracao DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    status VARCHAR(20)
-);
+INSERT INTO Usuario (nome, email, senha, cargo_agencia, status, fk_agencia)
+VALUES
+('Lucas', 'lucas@email.com', '12345678', 'Analista', 'Ativo', 1),
+('Mariana', 'mariana@email.com', '12345678', 'Analista', 'Ativo', 1),
+('Rafael', 'rafael@email.com', '12345678', 'Analista', 'Ativo', 1),
+('Beatriz', 'beatriz@email.com', '12345678', 'Analista', 'Ativo', 1),
+('Henrique', 'henrique@email.com', '12345678', 'Analista', 'Ativo', 1),
+('Letícia', 'leticia@email.com', '12345678', 'Analista', 'Ativo', 1),
+('Fernanda', 'fernanda@email.com', '12345678', 'Analista', 'Ativo', 1),
+('Eduardo', 'eduardo@email.com', '12345678', 'Analista', 'Ativo', 1),
+('Carolina', 'carolina@email.com', '12345678', 'Analista', 'Ativo', 1),
+('Gabriel', 'gabriel@email.com', '12345678', 'Analista', 'Ativo', 1);
 
 CREATE TABLE Municipio (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -79,7 +83,6 @@ INSERT INTO Municipio (id, nome_municipio) VALUES
 (8, 'Santos'),
 (9, 'São Vicente');
 
-
 CREATE TABLE Ocorrencias (
     id INT AUTO_INCREMENT PRIMARY KEY,
     tipo_ocorrencia VARCHAR(45),
@@ -94,37 +97,35 @@ CREATE TABLE Ocorrencias (
 
 CREATE TABLE Logs (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    mensagem LONGTEXT,
+    mensagem VARCHAR(500),
     tipo VARCHAR(45),
     dt_registro VARCHAR(45)
 );
 
 CREATE TABLE Administrador (
-	id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100),
     email VARCHAR(100),
     senha VARCHAR(100)
 );
 
-CREATE TABLE Filtro (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-    conteudo_filtro VARCHAR(45),
-    descricao VARCHAR(300),
-    dt_geracao DATETIME DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE Filtros (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(45),
+    data_criacao DATETIME,
+    descricao VARCHAR(45),
+    categoria VARCHAR(45),
+    fk_administrador INT,
+    FOREIGN KEY (fk_administrador) REFERENCES Administrador(id)
 );
 
-
-INSERT INTO Usuario (nome, email, senha, cargo_agencia, status, fk_agencia)
-VALUES
-('Lucas', 'lucas@email.com', '12345678', 'Analista', 'Ativo', 1),
-('Mariana', 'mariana@email.com', '12345678', 'Analista', 'Ativo', 1),
-('Rafael', 'rafael@email.com', '12345678', 'Analista', 'Ativo', 1),
-('Beatriz', 'beatriz@email.com', '12345678', 'Analista', 'Ativo', 1),
-('Henrique', 'henrique@email.com', '12345678', 'Analista', 'Ativo', 1),
-('Letícia', 'leticia@email.com', '12345678', 'Analista', 'Ativo', 1),
-('Fernanda', 'fernanda@email.com', '12345678', 'Analista', 'Ativo', 1),
-('Eduardo', 'eduardo@email.com', '12345678', 'Analista', 'Ativo', 1),
-('Carolina', 'carolina@email.com', '12345678', 'Analista', 'Ativo', 1),
-('Gabriel', 'gabriel@email.com', '12345678', 'Analista', 'Ativo', 1);
-
-select count(id) from Ocorrencias;
+CREATE TABLE Pedidos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    descricao VARCHAR(45),
+    status VARCHAR(30),
+    data_criacao DATETIME,
+    fk_usuario INT,
+    fk_administrador INT,
+    FOREIGN KEY (fk_usuario) REFERENCES Usuario(id),
+    FOREIGN KEY (fk_administrador) REFERENCES Administrador(id)
+);
