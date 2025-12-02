57dd32d7261c247279afb311267dab2600beba7a
Kevin Estrela <kevin.estrela@jazztech.com.br>
2025-12-02 15:47:18 -0300

    Criação de pedidos de geração de relatório

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
 var agenciaRouter = require("./src/routes/agencia");
 var dashboardRouter = require("./src/routes/dashboard");
+var pedidoRouter = require("./src/routes/pedido");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -30,6 +31,7 @@ app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/dashboard", dashboardRouter);
 app.use("/agencia", agenciaRouter);
+app.use("/pedido", pedidoRouter);
 
 app.listen(PORTA_APP, function () {
     console.log(`
